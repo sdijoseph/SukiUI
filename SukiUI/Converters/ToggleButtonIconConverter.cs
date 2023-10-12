62d934c93c308e07fa15d3da4e6385223ecd00ea
Stephen DiJoseph <sdijoseph@ctdi.com>
2023-10-12 11:44:22 -0400

    Add ToggleButtonIconConverter.cs

---
+++ SukiUI/Converters/ToggleButtonIconConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Material.Icons;
+
+namespace SukiUI.Converters;
+
+public class ToggleButtonIconConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool isChecked &&
+            targetType.IsAssignableTo(typeof(MaterialIconKind)))
+            return isChecked ? MaterialIconKind.ChevronLeft : MaterialIconKind.ChevronRight;
+
+        throw new NotSupportedException("Invalid use of {nameof(ToggleButtonIconConverter)}");
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
